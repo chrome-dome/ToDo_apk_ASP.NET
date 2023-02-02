0434038dfd1c12f86929af5a2b651398bf8ebdc8
chrome-dome <chrome-domepl@protonmail.com>
2023-02-02 11:40:37 +0100

    initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ToDoApp.Models
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public List<Issue> Issues { get; set; }
+	}
+
+	public enum IssueState
+	{
+		Todo,
+		Doing,
+		Done
+	}
+
+	class Issue
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Notes { get; set; }
+		public IssueState State { get; set; }
+		public bool IsUrgent { get; set; }
+		public DateTime? Deadline { get; set; }
+		public int? AssingedToId { get; set; }
+		public Person AssignedTo { get; set; }
+
+	}
+}
+
