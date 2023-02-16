20a15459d583e6b9a7acddc977a878e5c8184f13
chrome-dome <chrome-domepl@protonmail.com>
2023-02-16 19:28:32 +0100

    repair of the „urgent” indicator

--- Models/Issue.cs
+++ Models/Issue.cs
@@ -9,7 +9,7 @@ namespace ToDoApp.Models
             public string Title { get; set; } = "";
             public string Notes { get; set; } = "";
             public IssueState State { get; set; }
-            public bool? IsUrgent { get; set; }
+            public bool IsUrgent { get; set; }
             public DateTime? Deadline { get; set; }
             public int? AssingedToId { get; set; }
             public Person AssignedTo { get; set; } = Person.Empty;
