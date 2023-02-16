559defaeb243d194f8100cc4049074f5d397f7a4
chrome-dome <chrome-domepl@protonmail.com>
2023-02-16 20:46:13 +0100

    create new form update

--- Controllers/IssueController.cs
+++ Controllers/IssueController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
@@ -18,6 +19,16 @@ namespace ToDoApp.Controllers
 
         public IActionResult Create()
         {
+            var people = ToDoContext.Data.People.Select(p =>
+            {
+                var displayName = p.DisplayName != "" ? $"({p.DisplayName})" : "";
+
+                return new SelectListItem($"{p.Name} {p.Surame} {displayName}", p.Id.ToString());
+
+            });
+
+            ViewData["People"] = people;
+
             return View();
         }
     }
--- Models/IssueState.cs
+++ Models/IssueState.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoApp.Models
 {
         public enum IssueState
         {
+            [Display(Name = "Do zrobienia")]
             Todo,
+            [Display(Name = "W trakcie")]
             Doing,
+            [Display(Name = "Zrobione")]
             Done
         }
 }
